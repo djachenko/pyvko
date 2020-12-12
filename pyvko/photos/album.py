e5b4c97815192e5c57513b6f305f2662054474c1
Igor Djachenko <i.s.djachenko@gmail.com>
2020-12-13 06:24:09 +0700

    Довел до ума альбом (частично) Вынес логику стены и альбомов в миксины Развел загрузчики для стены и альбомов Добавил генератор для загрузки больших кусков

--- pyvko/photos/album.py
+++ pyvko/photos/album.py
@@ -1,12 +1,16 @@
-from typing import List
+from pathlib import Path
+from typing import List, Dict
 
 from vk import API
 
 from pyvko.api_based import ApiBased
+from pyvko.attachment.attachment import Attachment
 from pyvko.attachment.photo import Photo
+from pyvko.photos.photos_uploader import AlbumPhotoUploader
+from pyvko.shared.downloader import get_all
 
 
-class Album(ApiBased):
+class Album(ApiBased, Attachment):
     def __init__(self, api: API, api_object: dict) -> None:
         super().__init__(api)
 
@@ -18,29 +22,53 @@ class Album(ApiBased):
     def name(self) -> str:
         return self.__name
 
+    @property
+    def id(self) -> int:
+        return self.__id
+
     def get_photos(self) -> List[Photo]:
-        parameters = {
-            "owner_id": self.__owner_id,
-            "album_id": self.__id
-        }
+        parameters = self.get_request()
 
-        photos = []
+        photos_descriptions = get_all(parameters, self.api.photos.get)
 
-        while True:
-            parameters["offset"] = len(photos)
-
-            request = self.get_request(parameters)
-
-            response = self.api.photos.get(**request)
-
-            photos_descriptions = response["items"]
-            photos_count = response["count"]
-
-            photos_chunk = [Photo(photo_object) for photo_object in photos_descriptions]
-
-            photos += photos_chunk
-
-            if photos_count == len(photos):
-                break
+        photos = [Photo(photo_object) for photo_object in photos_descriptions]
 
         return photos
+
+    def get_request(self, parameters: Dict = None) -> dict:
+        parameters = parameters.copy()
+
+        parameters.update({
+            "owner_id": self.__owner_id,
+            "album_id": self.__id
+        })
+
+        return super().get_request(parameters)
+
+    def set_cover(self, cover: Photo):
+        request = self.get_request({
+            "photo_id": cover.id
+        })
+
+        self.api.photos.makeCover(**request)
+
+    def add_photo(self, path: Path) -> Photo:
+        uploader = AlbumPhotoUploader(self.api, self.id, -self.__owner_id)
+
+        return uploader.upload(path)
+
+    # region Attachment
+
+    @property
+    def type(self) -> str:
+        return "album"
+
+    @property
+    def owner_id(self) -> int:
+        return self.__owner_id
+
+    @property
+    def media_id(self) -> int:
+        return self.id
+
+    # endregion Attachment
